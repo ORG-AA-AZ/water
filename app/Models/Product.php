922e2a2c4b1d1427fbd5305ff93d762d98255144
abdelmajied94abusuliman <abdelmajied94abusuliman@users.noreply.github.com>
2024-10-16 15:12:36 +0000

    Fix styling issues (PHP-CS-Fixer)

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,7 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
-
     protected $fillable = [
         'name',
         'description',
@@ -16,7 +15,7 @@ class Product extends Model
         'price',
         'quantity',
         'factory',
-        'marketplace_id'
+        'marketplace_id',
     ];
 
     public function marketplace()
