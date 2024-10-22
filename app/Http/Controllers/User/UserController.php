56632f6870ec8e1194f4337536af268934041ca3
abdelmajied <abdelmajied.abusuliman@gmail.com>
2024-10-22 19:34:50 +0300

    wip

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -11,9 +11,7 @@ use App\Http\Requests\ChangeLocationRequest;
 use App\Http\Requests\ForgetPasswordRequest;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\NewVerifyCodeRequest;
-use App\Http\Requests\NewVerifyCodeRequest;
 use App\Http\Requests\ResetPasswordRequest;
-use App\Http\Requests\VerifyRequest;
 use App\Http\Requests\VerifyRequest;
 
 class UserController extends BaseAuthController
