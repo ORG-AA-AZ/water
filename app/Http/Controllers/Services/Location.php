c6c979ab5d5e218a1ad822e5e486c82222d48529
abdelmajied <abdelmajied.abusuliman@gmail.com>
2024-10-23 21:58:20 +0300

    add tests

--- app/Http/Controllers/Services/Location.php
+++ app/Http/Controllers/Services/Location.php
@@ -10,5 +10,12 @@ class Location
     public function changeLocation(ModelsEnum $model, ChangeLocationRequest $request)
     {
         $entity = $model->value::where('mobile', $request->input('mobile'))->first();
+
+        if(! $entity)
+        {
+            throw new \Exception('Mobile is not registered');
+        }
+
+        $entity->update(['latitude' => $request->latitude , 'longitude' => $request->longitude]);
     }
 }
